9ec27f6e089a8de77b4953ce5beceeec4ea258ec
hanxiaobo <hanxiaobo@zhiketong.cn>
2019-08-12 17:38:13 +0800

    命令行交互

--- bin/app-config-generator.js
+++ bin/app-config-generator.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 const getUrl = require('../lib/scheduling');
 
-getUrl(process.argv[3], process.argv[5])
+let url = process.argv[2].replace('*-api', process.argv[3])
+getUrl(url, process.argv[4], process.argv[5])
