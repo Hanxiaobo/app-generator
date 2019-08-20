9e2b595b3e648aff94cd7351f0f8fd77338a0ef4
admin <zhanglisha@zhiketong.cn>
2019-08-20 10:42:08 +0800

    修改

--- bin/app-config-generator.js
+++ bin/app-config-generator.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 const getUrl = require('../lib/scheduling');
-console.log(process.argv)
 let url = process.argv[2].replace('*-api', process.argv[3])
 getUrl(url, process.argv[4], process.argv[5])
