7f8eaf57f7bd28916cd3adbb3736351fa80dcf90
hanxiaobo <hanxiaobo@zhiketong.cn>
2019-12-09 15:27:41 +0800

    指定生成路径

--- bin/app-config-generator.js
+++ bin/app-config-generator.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 const getUrl = require('../lib/scheduling');
 let url = process.argv[2].replace('*-api', process.argv[3])
-getUrl(url, process.argv[4], process.argv[5], process.argv[3])
+getUrl(url, process.argv[4], process.argv[5], process.argv[3], process.argv[6])
